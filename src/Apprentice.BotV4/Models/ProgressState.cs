c0cf5c3940b7761d113e15dc0b50d39624f89379
Steve O'Hara <steve.ohara@digital.education.gov.uk>
2018-08-24 12:21:22 +0100

    [PF-74, PF-76] User status is now tracked from [notStarted] -> [inProgress] -> [complete || expired]

---
+++ src/Apprentice.BotV4/Models/ProgressState.cs
@@ -0,0 +1,31 @@
+﻿namespace ESFA.DAS.ProvideFeedback.Apprentice.BotV4.Models
+{
+    using System.Runtime.Serialization;
+
+    public enum ProgressState
+    {
+        /// <summary>
+        /// Feedback request has been sent but not started
+        /// </summary>
+        [EnumMember(Value = "notStarted")]
+        NotStarted,
+
+        /// <summary>
+        /// Feedback is in progress.
+        /// </summary>
+        [EnumMember(Value = "inProgress")]
+        Enagaged,
+
+        /// <summary>
+        /// Feedback has been completed
+        /// </summary>
+        [EnumMember(Value = "complete")]
+        Complete,
+
+        /// <summary>
+        /// Feedback cycle has expired before user completed the survey
+        /// </summary>
+        [EnumMember(Value = "expired")]
+        Expired
+    }
+}
