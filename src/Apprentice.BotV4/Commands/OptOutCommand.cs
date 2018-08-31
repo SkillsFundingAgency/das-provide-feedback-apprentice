19363967b29a9cd6a0fbbf36d48fddba189dca56
Steve O'Hara <steve.ohara@digital.education.gov.uk>
2018-08-31 17:29:48 +0100

    [PF-89] Opt out command added to terminate a conversation.

---
+++ src/Apprentice.BotV4/Commands/OptOutCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace ESFA.DAS.ProvideFeedback.Apprentice.BotV4.Commands
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Microsoft.Bot.Builder.Dialogs;
+
+    public sealed class OptOutCommand : UserCommand
+    {
+        public OptOutCommand()
+            : base("stop")
+        {   
+        }
+
+        public override async Task ExecuteAsync(DialogContext dc)
+        {
+            // TODO: Add to suppression list here
+            await dc.Context.SendActivity($"OK. You have opted out successfully.");
+            dc.EndAll();
+        }
+    }
+}
