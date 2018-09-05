5dc0621f3a16275fb6a5a064ace2a719c37b8f54
Steve O'Hara <github@soharadesign.com>
2018-09-05 15:12:34 +0100

    Fixed breaking changes with Function references (v2)

---
+++ src/Apprentice.Core/State/SurveyState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using ESFA.DAS.ProvideFeedback.Apprentice.Core.Models;
+using Newtonsoft.Json;
+
+namespace ESFA.DAS.ProvideFeedback.Apprentice.Core.State
+{
+    [Serializable]
+    public class SurveyState
+    {
+        public SurveyState()
+        {
+        }
+
+        public List<BinaryQuestionResponse> Responses { get; set; } = new List<BinaryQuestionResponse>();
+
+        public string SurveyId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public ProgressState Progress { get; set; }
+
+        public int Score => this.Responses.Sum(pqr => pqr?.Score ?? 0);
+    }
+}
---
+++ src/Apprentice.Core/State/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace ESFA.DAS.ProvideFeedback.Apprentice.Core.State
+{
+    /// <summary>
+    /// User state information.
+    /// </summary>
+    [Serializable]
+    public class UserInfo
+    {
+        public SurveyState SurveyState { get; set; }
+    }
+}
