56a13fab59289ad4b0b6ac6f12b4bfcdf815e45d
Steve O'Hara <github@soharadesign.com>
2018-09-25 18:15:52 +0100

    Added missing Features config

---
+++ src/Apprentice.Core/Configuration/Features.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Features.cs" company="Education & Skills Funding Agency">
+//   Copyright © 2018 Education & Skills Funding Agency
+// </copyright>
+// <summary>
+//   The azure config.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace ESFA.DAS.ProvideFeedback.Apprentice.Core.Configuration
+{
+    /// <summary>
+    /// Configuration for Azure.
+    /// </summary>
+    public class Features
+    {
+        /// <summary>
+        /// Adds a delay to bot responses, and a typing indicator to fake human input
+        /// </summary>
+        public bool RealisticTypingDelay { get; set; }
+
+        /// <summary>
+        /// Group multiple responses into a single response back to the user
+        /// </summary>
+        public bool CollateResponses { get; set; }
+    }
+}
