92f95ffb84a7353209b4b6f44702accb7627c769
Steve O'Hara <github@soharadesign.com>
2018-10-03 15:06:15 +0100

    [PF-153] Updated Dialogs to latest framework - removed redundant items

--- src/Apprentice.BotV4/Dialogs/InMemoryApprenticeFeedbackSurvey.cs
+++ src/Apprentice.BotV4/Dialogs/InMemoryApprenticeFeedbackSurvey.cs
@@ -23,54 +23,46 @@
         private const string QuestionsDaysOfTraining =
             "Over the last 6 months, have you received at least 25 days of training? Please type ‘yes’ or ‘no’";
 
-        private const string QuestionsOverallSatisfaction = 
-            "Overall, are you satisfied with your apprenticeship?";
+        private const string QuestionsOverallSatisfaction = "Overall, are you satisfied with your apprenticeship?";
 
-        private const string QuestionsTrainerKnowledge = 
-            "Next question, is your trainer good?";
+        private const string QuestionsTrainerKnowledge = "Next question, is your trainer good?";
 
-        private const string ResponsesItsReallyHelpful = 
-            "Great, thanks for your feedback. It’s really helpful";
+        private const string ResponsesItsReallyHelpful = "Great, thanks for your feedback. It’s really helpful";
 
-        private const string ResponsesNegative01 = 
-            "Okay, thanks for the feedback";
+        private const string ResponsesNegative01 = "Okay, thanks for the feedback";
 
-        private const string ResponsesNegative02 = 
-            "Okay, thanks for that";
+        private const string ResponsesNegative02 = "Okay, thanks for that";
 
-        private const string ResponsesPositive01 = 
-            "Thanks!";
+        private const string ResponsesPositive01 = "Thanks!";
 
-        private const string ResponsesPositive02 = 
-            "Thanks";
+        private const string ResponsesPositive02 = "Thanks";
 
-        private const string ResponsesSorryToHearThat = 
-            "Okay, sorry to hear that";
+        private const string ResponsesSorryToHearThat = "Okay, sorry to hear that";
 
         public InMemoryApprenticeFeedbackSurvey()
         {
             this.Id = "afb-v3";
             this.Steps = new List<ISurveyStep>()
-                             {
-                                 this.CreateStartStep(),
-                                 this.CreateQuestionOne(),
-                                 this.CreateQuestionTwo(),
-                                 this.CreateQuestionThree(),
-                                 this.CreateEndStep()
-                             };
+                {
+                    this.CreateStartStep(),
+                    this.CreateQuestionOne(),
+                    this.CreateQuestionTwo(),
+                    this.CreateQuestionThree(),
+                    this.CreateEndStep()
+                };
         }
 
         public InMemoryApprenticeFeedbackSurvey(string id)
         {
             this.Id = id;
             this.Steps = new List<ISurveyStep>()
-                             {
-                                 this.CreateStartStep(),
-                                 this.CreateQuestionOne(),
-                                 this.CreateQuestionTwo(),
-                                 this.CreateQuestionThree(),
-                                 this.CreateEndStep()
-                             };
+                {
+                    this.CreateStartStep(),
+                    this.CreateQuestionOne(),
+                    this.CreateQuestionTwo(),
+                    this.CreateQuestionThree(),
+                    this.CreateEndStep()
+                };
         }
 
         public string Id { get; set; }
@@ -81,29 +73,26 @@
         {
             var id = "feedback-end";
             var responses = new List<IResponse>
-                                {
-                                    new PredicateResponse()
-                                        {
-                                            Id = nameof(FinishSpeakToYourEmployer),
-                                            Predicate = u =>
-                                                u.SurveyState.Score < 300,
-                                            Prompt = FinishSpeakToYourEmployer
-                                        },
-                                    new PredicateResponse()
-                                        {
-                                            Id = nameof(FinishKeepUpTheGoodWork),
-                                            Predicate = u =>
-                                                u.SurveyState.Score >= 300,
-                                            Prompt = FinishKeepUpTheGoodWork
-                                        },
-                                    new PredicateResponse()
-                                        {
-                                            Id = nameof(FinishFormalComplaint),
-                                            Predicate =
-                                                u => u.SurveyState.Score < 0,
-                                            Prompt = FinishFormalComplaint
-                                        }
-                                };
+                {
+                    new PredicateResponse
+                        {
+                            Id = nameof(FinishSpeakToYourEmployer),
+                            Predicate = u => u.Score < 300,
+                            Prompt = FinishSpeakToYourEmployer,
+                        },
+                    new PredicateResponse
+                        {
+                            Id = nameof(FinishKeepUpTheGoodWork),
+                            Predicate = u => u.Score >= 300,
+                            Prompt = FinishKeepUpTheGoodWork,
+                        },
+                    new PredicateResponse
+                        {
+                            Id = nameof(FinishFormalComplaint),
+                            Predicate = u => u.Score < 0,
+                            Prompt = FinishFormalComplaint,
+                        },
+                };
             return new EndStep() { Id = id, Responses = responses };
         }
 
@@ -111,10 +100,10 @@
         {
             var id = "feedback-q1";
             var responses = new List<IResponse>
-                                {
-                                    new PositiveResponse { Prompt = ResponsesPositive01 },
-                                    new NegativeResponse { Prompt = ResponsesNegative01 }
-                                };
+                {
+                    new PositiveResponse { Prompt = ResponsesPositive01 },
+                    new NegativeResponse { Prompt = ResponsesNegative01 },
+                };
             var prompt = QuestionsDaysOfTraining;
             var score = 100;
 
@@ -125,10 +114,10 @@
         {
             var id = "feedback-q3";
             var responses = new List<IResponse>
-                                {
-                                    new PositiveResponse { Prompt = ResponsesItsReallyHelpful },
-                                    new NegativeResponse { Prompt = ResponsesSorryToHearThat }
-                                };
+                {
+                    new PositiveResponse { Prompt = ResponsesItsReallyHelpful },
+                    new NegativeResponse { Prompt = ResponsesSorryToHearThat },
+                };
             var prompt = QuestionsOverallSatisfaction;
             var score = 100;
 
@@ -139,10 +128,10 @@
         {
             var id = "feedback-q2";
             var responses = new List<IResponse>
-                                {
-                                    new PositiveResponse { Prompt = ResponsesPositive02 },
-                                    new NegativeResponse { Prompt = ResponsesNegative02 }
-                                };
+                {
+                    new PositiveResponse { Prompt = ResponsesPositive02 },
+                    new NegativeResponse { Prompt = ResponsesNegative02 },
+                };
             var prompt = QuestionsTrainerKnowledge;
             var score = 100;
 
@@ -153,18 +142,10 @@
         {
             var id = "feedback-start";
             var responses = new List<IResponse>
-                                {
-                                    new StaticResponse()
-                                        {
-                                            Id = nameof(IntroWelcome),
-                                            Prompt = IntroWelcome
-                                        },
-                                    new StaticResponse()
-                                        {
-                                            Id = nameof(IntroOptOut),
-                                            Prompt = IntroOptOut
-                                        }
-                                };
+                {
+                    new StaticResponse() { Id = nameof(IntroWelcome), Prompt = IntroWelcome },
+                    new StaticResponse() { Id = nameof(IntroOptOut), Prompt = IntroOptOut },
+                };
             return new StartStep() { Id = id, Responses = responses };
         }
     }
