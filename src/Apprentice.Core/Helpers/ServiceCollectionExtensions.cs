217039df97c5d7a7078fb5aebe7d93d7b28e1656
Steve O'Hara <steve.ohara@digital.education.gov.uk>
2018-11-06 11:47:05 +0000

    [PF-182] Updated service registration extensions to skip over abstract classes when using a list resolver

--- src/Apprentice.Core/Helpers/ServiceCollectionExtensions.cs
+++ src/Apprentice.Core/Helpers/ServiceCollectionExtensions.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace ESFA.DAS.ProvideFeedback.Apprentice.Core.Helpers
+﻿namespace ESFA.DAS.ProvideFeedback.Apprentice.Core.Helpers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    using Microsoft.Extensions.DependencyInjection;
+
     public static class ServiceCollectionExtensions
     {
         public static IServiceCollection RegisterAllTypes<T>(this IServiceCollection services, Assembly[] assemblies, ServiceLifetime lifetime = ServiceLifetime.Transient)
         {
-            var typesFromAssemblies = assemblies.SelectMany(a => a.DefinedTypes.Where(x => x.GetInterfaces().Contains(typeof(T))));
+            var typesFromAssemblies = assemblies.SelectMany(a => a.DefinedTypes.Where(x => x.GetInterfaces().Contains(typeof(T)) && !x.IsAbstract));
             foreach (var type in typesFromAssemblies)
             {
                 services.Add(new ServiceDescriptor(typeof(T), type, lifetime));
