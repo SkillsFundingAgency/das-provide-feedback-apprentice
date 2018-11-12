96f1ef395308c21a24eded3ac9f52df4e67abdb8
Corey <corey.faulconbridge@digital.education.gov.uk>
2018-11-12 10:35:45 +0000

    Pf-167 Db changes for phone number

--- src/ESFA.DAS.ProvideFeedback.Apprentice.Database/ApprenticeSurveyInvites.sql
+++ src/ESFA.DAS.ProvideFeedback.Apprentice.Database/ApprenticeSurveyInvites.sql
@@ -2,11 +2,11 @@
 (
 	[Id] UNIQUEIDENTIFIER NOT NULL,
 	[UniqueLearnerNumber] VARCHAR(50) NOT NULL , 
-    [MobileNumber] INT NOT NULL, 
+    [MobileNumber] VARCHAR(15) NOT NULL, 
 	[StandardCode] INT NOT NULL,
-    [Ukprn] INT NOT NULL, 
+    [Ukprn] BIGINT NOT NULL, 
     [SurveyCode] VARCHAR(10) NOT NULL, 
     [SentDate] DATETIME NULL, 
-    [ExpiryDate] DATETIME NOT NULL, 
+    [ExpiryDate] DATETIME NULL, 
     CONSTRAINT [PK_ApprenticeSurveyInvites] PRIMARY KEY ([Id])
 )
