58c238976ca30d4c8a2a134cb8cd72c5ba159f60
Steve O'Hara <steve.ohara@digital.education.gov.uk>
2018-11-27 10:26:09 +0000

    [PF-185] added appsettings.json template to function

--- src/Apprentice.Functions.NotifyMessageHandlerV2/Services/SettingsProvider.cs
+++ src/Apprentice.Functions.NotifyMessageHandlerV2/Services/SettingsProvider.cs
@@ -1,11 +1,14 @@
 ﻿namespace ESFA.DAS.ProvideFeedback.Apprentice.Functions.NotifyMessageHandlerV2.Services
 {
+    using System;
+    using System.IO;
+
     using Microsoft.Azure.WebJobs;
     using Microsoft.Extensions.Configuration;
 
     public class SettingsProvider : ISettingService
     {
-        private readonly IConfigurationRoot config;
+        private readonly IConfigurationRoot configuration;
 
         /// <summary>
         ///     Initializes a new instance of the <see cref="SettingsProvider"/> class.
@@ -13,17 +16,29 @@
         /// <param name="ctx"> The <see cref="ExecutionContext"/>. </param>
         public SettingsProvider(ExecutionContext ctx)
         {
-            this.config = new ConfigurationBuilder()
+            this.configuration = new ConfigurationBuilder()
                 .SetBasePath(ctx.FunctionAppDirectory)
                 .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddEnvironmentVariables()
                 .Build();
         }
 
         public string Get(string parameterName)
         {
-            var parameter = this.config[parameterName];
+            var parameter = this.configuration[parameterName];
             return parameter;
+        }
+
+        public int GetInt(string parameterName)
+        {
+            string parameter = this.Get(parameterName);
+            if (!int.TryParse(parameter, out int result))
+            {
+                throw new Exception($"Configuration variable {parameterName} could not be cast to an integer");
+
+            }
+            return result;
         }
     }
 }
--- src/Apprentice.Functions.NotifyMessageHandlerV2/Startup.cs
+++ src/Apprentice.Functions.NotifyMessageHandlerV2/Startup.cs
@@ -1,35 +1,43 @@
-﻿using System.Data;
-using System.Data.SqlClient;
-using System.IO;
-using ESFA.DAS.ProvideFeedback.Apprentice.Data.Repositories;
-using ESFA.DAS.ProvideFeedback.Apprentice.Functions.NotifyMessageHandlerV2;
-using ESFA.DAS.ProvideFeedback.Apprentice.Functions.NotifyMessageHandlerV2.DependecyInjection.Config;
-using Microsoft.Azure.WebJobs;
+﻿using ESFA.DAS.ProvideFeedback.Apprentice.Functions.NotifyMessageHandlerV2;
+
 using Microsoft.Azure.WebJobs.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 
 [assembly: WebJobsStartup(typeof(Startup))]
+
 namespace ESFA.DAS.ProvideFeedback.Apprentice.Functions.NotifyMessageHandlerV2
 {
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.IO;
+
+    using ESFA.DAS.ProvideFeedback.Apprentice.Data.Repositories;
+    using ESFA.DAS.ProvideFeedback.Apprentice.Functions.NotifyMessageHandlerV2.DependecyInjection.Config;
+
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
     internal class Startup : IWebJobsStartup
     {
-        private readonly IConfigurationRoot _configuration;
+        private readonly IConfigurationRoot configuration;
+
         public Startup()
         {
-            _configuration = new ConfigurationBuilder()
+            this.configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddEnvironmentVariables()
                 .Build();
         }
 
-        public void Configure(IWebJobsBuilder builder) =>
-            builder.AddDependencyInjection(ConfigureServices);
+        public void Configure(IWebJobsBuilder builder) => builder.AddDependencyInjection(this.ConfigureServices);
 
         private void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IDbConnection>(c => new SqlConnection(_configuration.GetConnectionStringOrSetting("SqlConnectionString")));
+            services.AddTransient<IDbConnection>(c => new SqlConnection(this.configuration.GetConnectionStringOrSetting("SqlConnectionString")));
             services.AddScoped<IStoreApprenticeSurveyDetails, ApprenticeSurveyInvitesRepository>();
         }
     }
