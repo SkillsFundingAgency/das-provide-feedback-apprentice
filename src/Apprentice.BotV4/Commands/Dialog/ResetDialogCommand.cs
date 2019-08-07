65ecf02d42b8ac5b08c85ea3829f3bf3429d368c
Corey <corey.faulconbridge@digital.education.gov.uk>
2019-08-07 12:01:37 +0100

    Allow reset command response through the activity middleware

--- src/Apprentice.BotV4/Commands/Dialog/ResetDialogCommand.cs
+++ src/Apprentice.BotV4/Commands/Dialog/ResetDialogCommand.cs
@@ -8,7 +8,7 @@
     using ESFA.DAS.ProvideFeedback.Apprentice.Core.State;
 
     using Microsoft.Bot.Builder.Dialogs;
-
+    using Microsoft.Bot.Schema;
     using BotConfiguration = ESFA.DAS.ProvideFeedback.Apprentice.Core.Configuration.Bot;
 
     public sealed class ResetDialogCommand : AdminCommand, IBotDialogCommand
@@ -27,8 +27,15 @@
             userProfile.SurveyState = new SurveyState();
 
             await this.state.ConversationState.ClearStateAsync(dc.Context, cancellationToken);
+            var activity = new Activity
+            {
+                Type = ActivityTypes.Message,
+                Id = "Reset",
+                Text = $"OK. Resetting conversation...",
+                ChannelData = dc.Context.Activity.ChannelData
+            };
 
-            await dc.Context.SendActivityAsync($"OK. Resetting conversation...", cancellationToken: cancellationToken);
+            await dc.Context.SendActivityAsync(activity, cancellationToken: cancellationToken);
             return await dc.CancelAllDialogsAsync(cancellationToken);
         }
     }
